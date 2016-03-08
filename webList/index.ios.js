0b291006b7d2cdc6bad79440a59ae7449666764c
Goodspeeds <goodspeeds@Lab205bdeiMac-2.local>
2016-03-08 16:02:12 +0800

    Goodspeed

--- webList/index.ios.js
+++ webList/index.ios.js
@@ -3,8 +3,7 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-//var Header = require('./header');
-import Header from './header';
+var Header = require('./header');
 import React, {
   AppRegistry,
   Component,
@@ -16,17 +15,47 @@ import React, {
 class webList extends Component {
   render() {
     return (
-      <View style={styles.flex}>
-        <Header></Header>
-      </View>
+      <view style={styles.flex}>
+      <Header></Header>
+      </view>
     );
   }
 }
+
+class List extends Component{
+  render(){
+    return({
+    <View style={styles.list_item}>
+       <Text style={styles.list_item_font}>{this.props.title}</Text>
+    </View>
+    });
+  }
+}
+
+
+
+
+
 
 const styles = StyleSheet.create({
  flex:{
   flex:1
  },
+
+list_item:{
+  height:40,
+  marginLeft:10,
+  marginRight:10,
+  borderBottowWidth:1,
+  borderBottowColor:'#add',
+  justifyContent:'center'
+},
+
+list_item_font:{
+  fontSize
+}
+
+
 });
 
 AppRegistry.registerComponent('webList', () => webList);
